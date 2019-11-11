11cf235c992b30c08fcdc44c79a50d0cb3f9ea84
George <george.flecknell@hotmail.com>
2019-11-11 16:32:03 +0000

    Update main.js

--- main.js
+++ main.js
@@ -1,2 +1,3 @@
 document.write("Hi!</br>");
 document.write("<p>This is my presence on the internet</p><p>Lets keep this simple. Music will get published here eventually. That's it really.</p>");
+document.write("<p>This is <a link='smallWorld/sw.html'>smallworld</a></p>");
