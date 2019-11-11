34e8b44350ad324068970a6700d982616205f281
George <george.flecknell@hotmail.com>
2019-11-11 14:57:00 +0000

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,2 @@
-document.write("Hi!");
+document.write("Hi!</br>");
+document.write("<p>This is my presence on the internet</p><p>Lets keep this simple. Music will get published here eventually. That's it really.</p>");
