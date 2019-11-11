0eab67a299a065221a3f7b6c87a101c26714cee5
George <george.flecknell@hotmail.com>
2019-11-11 16:17:51 +0000

    Update main.js

--- main.js
+++ main.js
@@ -1,2 +1,2 @@
-document.write("Hi!</br>");
-document.write("<p>This is my presence on the internet</p><p>Lets keep this simple. Music will get published here eventually. That's it really.</p>");
+document.write(("Hi!</br>")toUpperCase());
+document.write(("<p>This is my presence on the internet</p><p>Lets keep this simple. Music will get published here eventually. That's it really.</p>")toUpperCase());
