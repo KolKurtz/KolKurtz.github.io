10f64795eb62596f2ae08c51c8684a4cf93bf409
George <george.flecknell@hotmail.com>
2020-03-11 08:23:17 +0000

    Surveys update

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
-document.write("Hi!</br>");
-document.write("<p>This is my presence on the internet</p><p>Lets keep this simple. Music will get published here eventually. That's it really.</p>");
+document.write("Hello.</br>");
+document.write("<p>This is my presence on the internet. Music will get published here eventually, some articles and apps appear below. That's it really. Contact at kolkurtz@gmx.com.</p>");
+document.write("<br><p>APPS</p><br>");
 document.write("<p>This is <a href='smallWorld/sw.html'>smallworld</a></p>");
 document.write("<p>Practice key <a href='RK/randKey.html'>randomiser</a></p>");
 document.write("<p>Design chords <a href='cDes/cDes.html'>here</a></p>");
+document.write("<br><p>BLOG</p><br>");
