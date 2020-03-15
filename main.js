1d3973c5d0f03a3462466230b120941f628a7e3a
George <george.flecknell@hotmail.com>
2020-03-15 12:46:33 +0000

    Restyle

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 document.write("Hello.</br>");
-document.write("<p>This is my presence on the internet. Music will get published here eventually, some articles and apps appear below. Contact at kolkurtz@gmx.com.</p>");
+document.write("<p>This is my presence on the internet. Music will get published here eventually, some articles and apps appear below.</p>");
 document.write("<br><p>APPS</p><br>");
 document.write("<p>This is <a href='smallWorld/sw.html'>smallworld</a></p>");
 document.write("<p>Practice key <a href='RK/randKey.html'>randomiser</a></p>");
