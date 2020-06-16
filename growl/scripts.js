c706eb3186f1c3eeec26ba1114dfcabe442c091e
George <george.flecknell@hotmail.com>
2020-06-16 11:59:56 +0100

    Post updates
    Google removed, Fb removed

--- growl/scripts.js
+++ growl/scripts.js
@@ -5,11 +5,13 @@ const somebody = document.getElementById('cats');
 const mainy = document.getElementById('main')
 
 const categs = {
-                "THEORY" : ["TH1_CHORDEXT1.html","ART2"],
-                "GEAR": ["ART1","ART2","ART3"],
-                "OPINION": ["OP1_WELCOME.html","OP2_20THANNIVERSARY.html","ART3","ART4"],
-                "PRACTICE": ["PR1_MOSTUSEFULEXERCISE.html","ART2","ART3","ART4","ART5"],
-                "COMPOSITION": ["ART1","ART2","ART3","ART4","ART5","ART6"]
+                "THEORY" : ["TH1_CHORD^EXTENSION.html","TH2_UNCAGED.html","TH3_INVERSION101.html","TH4_INVERSIONS.html","TH5_NEAPOLITAN^CHORDS.html","TH6_KEYS^MODES^BISCUITS.html","TH7_NUMERIC^NOTATION.html","TH8_CHORD^ANATOMY^1.html","TH9_CHORD^ANATOMY^2.html",
+                "TH10_FIRST^GUITAR^CHORDS.html","TH11_FUNCTION.html","TH12_ARP^WHAT^ARE^YOU.html","TH13_SEVENTH^CHORDS.html"],
+                "RECORD": ["RE1_CARCASSI^CAPRICE^OP26^NO3.html"],
+                "OPINION": ["OP1_WELCOME.html","OP2_20TH^ANNIVERSARY.html"],
+                "PRACTICE": ["PR1_MOST^USEFUL^EXERCISE.html","PR2_ARPEGGIO^SYNCHRONOUS.html","PR3_MODAL^MELANGE.html","PR4_BREAK^INTO^COMPING.html","PR5_DOWNWARDS^DOG.html","PR6_ARPEGGIO^SNAKES.html","PR7_HANON^FOR^GUITAR.html","PR8_INVERSION^EXCURSION.html","PR9_A^CHORD^MODE^EXERCISE.html"],
+                "COMPOSITION": ["CO1_SIMPLIFY^PROGRESSIONS.html"],
+                "TAB": ["TB1_BACH^GAVOTTE^GR3.pdf"]
               }
 
 
@@ -26,11 +28,18 @@ function showArts(catIn)
   for(i=0;i<listy.length;i++)
   {
     var cutString = listy[i].split("_")[1].split(".")[0];
-    mainy.innerHTML += "<li><a href='post/" + listy[i] + "'>" + cutString + "</a></li>";
+    mainy.innerHTML += "<li><a href='post/" + listy[i] + "'>" + charep(cutString) + "</a></li>";
   }
 
   mainy.innerHTML += "</ul>";
 
+}
+
+
+function charep(stringy)
+{
+  var inString = stringy.replace(/\^/g," ");
+  return inString;
 }
 
 
