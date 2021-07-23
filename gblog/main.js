b3b9a836af144c96b0d891ae455db7d7d598042c
George <george.flecknell@hotmail.com>
2021-07-23 16:53:01 +0100

    v0.2
    Added new posts and the post loader method

--- gblog/main.js
+++ gblog/main.js
@@ -1,9 +1,24 @@
+//GBLOG MAIN KOLKURTZ 2021
+//TODO
+//Hovermode toggle - zoom images
+//Posts loader
+//Post editor
+
+
 
 //MAIN    
 
 function runMain()
 {
     $('.modal').hide();
+
+    //load posts
+    jQuery.get('posts/posts.txt', function(data) 
+    {
+        alert(data);
+    });
+
+
     //check for local storage
     if(localStorage.getItem('test')) 
     {
