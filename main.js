fa356d0b8624ffc39e617ad70ae9fcf00aedd381
George <george.flecknell@hotmail.com>
2024-09-18 13:44:02 +0100

    Go live of redesign
    Changed index file

--- main.js
+++ main.js
@@ -1,7 +1,1 @@
-document.write("<div class='level1'>Hello.</br>");
-document.write("<p>This is my presence on the internet. Music will get published here eventually, some articles and apps appear below.</p></div>");
-document.write("<div class='level1'><p>APPS</p>");
-document.write("<p>This is <a href='smallWorld/sw.html'>smallworld</a></p>");
-document.write("<p>French grammar practice <a href='conjug/conjug.html'>app</a></p>");
-document.write("<p>Practice key <a href='RK/randKey.html'>randomiser</a></p>");
-document.write("<p>Design chords <a href='cDes/cDes.html'>here</a></p></div>");
+const pubapps = ["smallworld", "fGrammar", "keyRand", "cDes"];
